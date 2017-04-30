1dc969b1f4f5be2a385632dfaebc1f6db9a83759
unknown <zhangkang103@gmail.com>
2017-04-30 23:47:18 +0800

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div>React</div>
+        <div>hello!</div>
+      </div>
+    )
+  }
+}
+
+export default App
