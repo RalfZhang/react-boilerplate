0f881ec3beb83b01782f4c236c1b9025a274b8a8
RalfZhang <zhangkang103@gmail.com>
2017-05-04 23:37:53 +0800

    update file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react'
+import styles from './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
+      <div className={styles.app}>
         <div>React</div>
         <div>hellooo!</div>
       </div>
