3a4f7db1ecacde6284bc2866dbb640721b7facf9
RalfZhang <zhangkang103@gmail.com>
2017-05-05 23:43:57 +0800

    hot mod

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
+import { AppContainer } from 'react-hot-loader'
 import App from './App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+const render =(Component) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('app')
+  )
+
+}
+
+render(App)
+
+
+// 模块热替换的 API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App)
+  });
+}
