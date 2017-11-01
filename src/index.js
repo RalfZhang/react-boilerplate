b59e2d1430ed6160ff69690967f4a518175240ea
RalfZhang <zhangkang103@gmail.com>
2017-11-01 18:25:32 +0800

    feature: update css and file import

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 // import _ from 'lodash';
-// import {aobj} from './a';
 const b = require('./b');
+// import {aobj} from './a';
 import a from './a';
+import './style.css';
+import Bg from './a.jpg';
+
 
 console.log('[index.js start]')
 function component() {
+
+
+
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.innerHTML = 'hello' + a()+b.bfun() + a()+b.bfun();
+  element.classList.add('hello');
   console.log('obj'+ b.bobj.def)
+  var myBg = new Image();
+  myBg.src = Bg;
+  element.appendChild(myBg);
   return element;
 }
+
+// console.log('[c] '+c.str());
 
 document.body.appendChild(component());
