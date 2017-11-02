805573c25f03f61fc063ec7a153cd753f7bfefbc
RalfZhang <zhangkang103@gmail.com>
2017-11-02 23:57:30 +0800

    feature: clear import

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 // import _ from 'lodash';
-const b = require('./b');
-// import {aobj} from './a';
-import a from './a';
-import './style.css';
-import Bg from './a.jpg';
+// const b = require('./b');
+// import a from './a';
+// import './style.css';
+// import Bg from './a.jpg';
 
+import printMe from './print';
 
 console.log('[index.js start]')
 function component() {
@@ -12,15 +12,23 @@ function component() {
 
 
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, now imported by this script
   // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.innerHTML = 'hello' + a()+b.bfun() + a()+b.bfun();
-  element.classList.add('hello');
-  console.log('obj'+ b.bobj.def)
-  var myBg = new Image();
-  myBg.src = Bg;
-  element.appendChild(myBg);
+
+  element.innerHTML = 'divvvvv'
+  // element.innerHTML = 'hello' + a()+b.bfun() + a()+b.bfun();
+  // element.classList.add('hello');
+  // console.log('obj'+ b.bobj.def)
+  // var myBg = new Image();
+  // myBg.src = Bg;
+
+  // element.appendChild(myBg);
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
