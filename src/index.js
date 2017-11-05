95b95685341fe2b9b8c6e6b5c606083492e21dea
RalfZhang <zhangkang103@gmail.com>
2017-11-05 22:34:33 +0800

    fix: update examle

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import _ from 'lodash';
+import _ from 'lodash';
 // const b = require('./b');
 // import a from './a';
 // import './style.css';
@@ -15,9 +15,9 @@ function component() {
   var btn = document.createElement('button');
 
   // Lodash, now imported by this script
-  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  element.innerHTML = 'divvvvv'
+  // element.innerHTML = 'divvvvv'
   // element.innerHTML = 'hello' + a()+b.bfun() + a()+b.bfun();
   // element.classList.add('hello');
   // console.log('obj'+ b.bobj.def)
