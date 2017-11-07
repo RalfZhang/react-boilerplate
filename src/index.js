496644008c365de5bf218b906dbfbf298c1acc47
RalfZhang <zhangkang103@gmail.com>
2017-11-07 18:23:09 +0800

    feature: add dev and prod test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-import _ from 'lodash';
+// import _ from 'lodash';
+import { cube } from './math.js';
 import printMe from './print.js';
-import './style.css';
+// import './style.css';
+
+// if (process.env.NODE_ENV !== 'production') {
+//   console.log('---Looks like we are in development mode!');
+// }
 
 function component() {
   var element = document.createElement('div');
   var btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML ='abc'
 
   btn.innerHTML = 'Click me and check the console!';
   btn.onclick = printMe;
@@ -15,6 +21,13 @@ function component() {
 
   return element;
 }
+var preElem = document.createElement('pre');
+preElem.innerHTML= [
+  'Hello webpack',
+  '5 cubed is equal to '+cube(5)
+].join('\n\n');
+document.body.appendChild(preElem);
+
 
 let element = component();
 document.body.appendChild(element);
