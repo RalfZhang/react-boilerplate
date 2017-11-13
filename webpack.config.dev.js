fadf33452a3504d7af95badb4ba39da4c668a98c
RalfZhang <zhangkang103@gmail.com>
2017-11-13 23:52:45 +0800

    feature: add webpack-dev-server config in dev.config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,13 +5,18 @@ const common = require('./webpack.common.js');
 
 const config = {
   devtool: 'inline-source-map',
+  // 使用 webpack-dev-server 时候应应用此类配置项
+  // devServer: {
+  //   contentBase: './dist',
+  //   hot: true,
+  // },
+  // output: {
+  //   publicPath: '/',
+  // },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
   ],
-  // output: {
-  //   publicPath: '/',
-  // }
 };
 
 module.exports = merge(common, config);
