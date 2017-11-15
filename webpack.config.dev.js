e17c1846c18f1b68066f93dccf6bc7a954905bc7
RalfZhang <zhangkang103@gmail.com>
2017-11-15 17:52:24 +0800

    feature: add dev-server config to default

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -6,13 +6,14 @@ const common = require('./webpack.common.js');
 const config = {
   devtool: 'cheap-module-eval-source-map',
   // 使用 webpack-dev-server 时候应应用此类配置项
-  // devServer: {
-  //   contentBase: './dist',
-  //   hot: true,
-  // },
-  // output: {
-  //   publicPath: '/',
-  // },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+    hotOnly: false,
+  },
+  output: {
+    publicPath: '/',
+  },
   plugins: [
     new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
