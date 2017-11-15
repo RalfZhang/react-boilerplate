e1614a40dfc3e6ac61d0b1660c13921e7f220bcb
RalfZhang <zhangkang103@gmail.com>
2017-11-15 18:12:01 +0800

    feature: add friendly-errors-webpack-plugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+
 
 const config = {
   devtool: 'cheap-module-eval-source-map',
@@ -13,11 +15,13 @@ const config = {
     open: true,
     proxy: {},
     publicPath: '/',
+    quiet: true, // necessary for FriendlyErrorsPlugin (未配置)
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
+    new FriendlyErrorsPlugin(),
   ],
 };
 
