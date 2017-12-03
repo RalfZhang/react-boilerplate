115bb81cc1fae1349c2649ae9e21fe1538246e1a
RalfZhang <zhangkang103@gmail.com>
2017-12-03 23:58:47 +0800

    feature: import react

--- src/index.js
+++ src/index.js
@@ -1,53 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import _ from 'lodash';
-import { cube } from './math.js';
-import printMe from './print.js';
 import './style.css';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   console.log('---Looks like we are in development mode!');
-// }
+const element = (
+  <h1>
+    Hellot
+  </h1>
+);
 
-function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
-  var btn2 = document.createElement('button');
-  var br = document.createElement('br');
-
-  btn2.innerHTML='Click to output console';
-  btn2.onclick = printMe;
-  
-  btn.innerHTML = 'Click me and check the console!';
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.innerHTML ='abc';
-
-  element.appendChild(br);
-  element.appendChild(btn);
-  element.appendChild(btn2);
-  
-  // btn.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-  //   var print = module.default;
-  //   print();
-  // })
-  
-  return element;
-}
-var preElem = document.createElement('pre');
-preElem.innerHTML= [
-  'Hello webpack',
-  '5 cubed is equal to '+cube(5)
-].join('\n\n');
-document.body.appendChild(preElem);
-
-
-document.body.appendChild(component());
-
-
-// console.log('module', module);
-// if(module.hot) {
-//   module.hot.accept('./print.js', function(){
-//     console.log('Accepting the updated printMe module!')
-//     document.body.removeChild(element);
-//     element = component();
-//     document.body.appendChild(element);
-//   })
-// }
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
